6d4bddfa78d2f2e90a77d6600de1e36531d4e97c
Edwin Horneij <edwin@astekwallcovering.com>
2015-10-28 12:05:50 -0700

    Everything seems to be working

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,5 +1,9 @@
 Spree::HomeController.class_eval do
-  def index
-    @secondary_carousel = Spree::SecondaryCarousel.where(:page_url => request.original_fullpath, :active => true).first
-  end
+  before_action :load_secondary_carousel, only: :index
+
+  private
+
+    def load_secondary_carousel
+      @secondary_carousel = Spree::SecondaryCarousel.where(:page_url => request.original_fullpath, :active => true).first
+    end
 end
