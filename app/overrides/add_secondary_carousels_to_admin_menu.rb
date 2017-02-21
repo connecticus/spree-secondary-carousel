7a14e1624d70c736cc6fb17e573bb952c0f4d5eb
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-21 14:44:30 -0800

    Update original for override

--- app/overrides/add_secondary_carousels_to_admin_menu.rb
+++ app/overrides/add_secondary_carousels_to_admin_menu.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(
-    :original => 'b511780df47e0ebcdfe1aeb87c0143df8d2f4985',
-    :virtual_path => 'spree/layouts/admin',
-    :name => 'add_secondary_carousels_to_admin_menu',
-    :insert_bottom => '[data-hook="admin_tabs"]',
-    :text => '<ul class="nav nav-sidebar">
-        <%= tab Spree.t(:secondary_carousels), :url => admin_secondary_carousels_path, :icon => \'arrow-right\' %>
+    original: 'f87a7f6f2f0150a62576f88c40f33e2d77669a10',
+    virtual_path: 'spree/layouts/admin',
+    name: 'add_secondary_carousels_to_admin_menu',
+    insert_bottom: '[data-hook="admin_tabs"]',
+    text: '<ul class="nav nav-sidebar">
+        <%= tab Spree.t(:secondary_carousels), url: admin_secondary_carousels_path, icon: \'arrow-right\' %>
       </ul>'
 )
